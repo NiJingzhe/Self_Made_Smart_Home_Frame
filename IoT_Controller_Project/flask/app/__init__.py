1ce664f111cc8e764fa57ecd4bd11cb3087cff56
Ni Jingzhe <1448976235@qq.com>
2021-07-30 21:48:44 +0800

    modified:   IoT_Controller_Project/flask/app/__init__.py

--- IoT_Controller_Project/flask/app/__init__.py
+++ IoT_Controller_Project/flask/app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import os
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Home_Info.db')
+app.config['SQLALCHEMY_MIGRATE_REPO'] = os.path.join(basedir, 'db_repository')
 
 from app import routes
