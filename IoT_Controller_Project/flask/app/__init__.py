5b6b7954ee193142c00ae458bec3499345416227
Ni Jingzhe <1448976235@qq.com>
2021-07-30 19:10:25 +0800

    modified:   IoT_Controller_Project/flask/app/__init__.py 	IoT_Controller_Project/flask/app/db_model.py

--- IoT_Controller_Project/flask/app/__init__.py
+++ IoT_Controller_Project/flask/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+from app import db_model
 from app import routes
