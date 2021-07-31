e4bd3167940bf7ea09a9072ee88760e75a3f1dd2
Ni Jingzhe <1448976235@qq.com>
2021-07-31 23:36:39 +0800

    modified:   IoT_Controller_Project/flask/app/Home_Info.db 	modified:   IoT_Controller_Project/flask/app/home.py

--- IoT_Controller_Project/flask/app/home.py
+++ IoT_Controller_Project/flask/app/home.py
@@ -1,4 +1,5 @@
-
+from app.__init__ import device_in_home
+from app.__init__ import db
 class home:
 
     def __init__(self, owner_):
@@ -7,8 +8,16 @@ class home:
 
     def add_device(self, device):
         if not (device.name in self.device_list.keys()):
+            dev = device_in_home(device.name,device.control_type)
+            db.session.add(dev)
+            db.session.commit()
             self.device_list[device.name] = device.control_type
+            
 
     def del_device(self, device_name):
         if device_name in self.device_list.keys():
+            dev = device_in_home.query.filter_by(device_name = device_name).first()
+            db.session.delete(dev)
+            db.session.commit()
             del self.device_list[device_name]
+            
