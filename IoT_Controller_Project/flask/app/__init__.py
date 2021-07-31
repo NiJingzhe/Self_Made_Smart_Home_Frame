a3c7e25d370e59fd4af01f896d8a02db58a476ed
Ni Jingzhe <1448976235@qq.com>
2021-07-31 19:48:08 +0800

    small change in task logic

--- IoT_Controller_Project/flask/app/__init__.py
+++ IoT_Controller_Project/flask/app/__init__.py
@@ -15,15 +15,13 @@ from flask_sqlalchemy import SQLAlchemy as sqlal
 db = sqlal(app)
 
 
-class device_in_room(db.Model):
+class device_in_home(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    room_name = db.Column(db.String(255))
     device_name = db.Column(db.String(255))
     control_type = db.Column(db.String(255))
 
-    def __init__(self, device_name, control_type, room_name):
+    def __init__(self, device_name, control_type):
 
-        self.room_name = room_name
         self.device_name = device_name
         self.control_type = control_type
 
