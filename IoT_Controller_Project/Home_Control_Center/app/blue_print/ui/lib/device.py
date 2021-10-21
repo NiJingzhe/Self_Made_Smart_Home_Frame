e0160e8157fa9e698033a25b2e62984e09401a58
NiJingzhe <1448976235@qq.com>
2021-10-21 22:29:10 +0800

    new file:   __pycache__/Home_Control_Center.cpython-38.pyc 	new file:   __pycache__/__init__.cpython-38.pyc 	new file:   app/__pycache__/Data_Base.cpython-38.pyc 	new file:   app/__pycache__/__init__.cpython-38.pyc 	new file:   app/__pycache__/device.cpython-38.pyc 	new file:   app/__pycache__/home.cpython-38.pyc 	new file:   app/__pycache__/receiver.cpython-38.pyc 	new file:   app/__pycache__/routes.cpython-38.pyc 	new file:   app/__pycache__/sender.cpython-38.pyc 	new file:   app/blue_print/__pycache__/__init__.cpython-38.pyc 	new file:   app/blue_print/api/__pycache__/__init__.cpython-38.pyc 	new file:   app/blue_print/api/__pycache__/views.cpython-38.pyc 	new file:   app/blue_print/api/lib/__init__.py 	new file:   app/blue_print/api/lib/__pycache__/__init__.cpython-38.pyc 	new file:   app/blue_print/api/lib/__pycache__/receiver.cpython-38.pyc 	new file:   app/blue_print/api/lib/__pycache__/sender.cpython-38.pyc 	new file:   app/blue_print/api/lib/receiver.py 	new file:   app/blue_print/api/lib/sender.py 	new file:   app/blue_print/error/__pycache__/__init__.cpython-38.pyc 	new file:   app/blue_print/error/__pycache__/views.cpython-38.pyc 	new file:   app/blue_print/error/templates/lack_info_err.html 	new file:   app/blue_print/ui/__pycache__/__init__.cpython-38.pyc 	new file:   app/blue_print/ui/__pycache__/views.cpython-38.pyc 	new file:   app/blue_print/ui/lib/__init__.py 	new file:   app/blue_print/ui/lib/__pycache__/__init__.cpython-38.pyc 	new file:   app/blue_print/ui/lib/__pycache__/device.cpython-38.pyc 	new file:   app/blue_print/ui/lib/__pycache__/home.cpython-38.pyc 	new file:   app/blue_print/ui/lib/device.py 	new file:   app/blue_print/ui/lib/home.py 	new file:   app/blue_print/ui/templates/home.html 	new file:   app/blue_print/ui/templates/switch.html 	new file:   app/lib/Data_Base.py 	new file:   app/lib/__init__.py

---
+++ IoT_Controller_Project/Home_Control_Center/app/blue_print/ui/lib/device.py
@@ -0,0 +1,8 @@
+
+class device():
+    
+    def __init__(self,name,control_type):
+        self.name = name
+        self.control_type = control_type
+
+            
---
+++ IoT_Controller_Project/Home_Control_Center/app/lib/Data_Base.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy as sqlal
+from app import db
+
+
+class device_in_home(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    device_name = db.Column(db.String(255))
+    control_type = db.Column(db.String(255))
+
+    def __init__(self, device_name, control_type):
+
+        self.device_name = device_name
+        self.control_type = control_type
+
+    def __repr__(self):
+
+        return '<Device %r %r>' % self.device_name % self.control_type
+
+
+db.create_all()
