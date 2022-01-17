e04543869260f580161317f84fc864faecf93614
倪旌哲 <1448976235@qq.com>
2022-01-17 23:27:48 +0800

    修改：     ../Device_Driver/.vscode/c_cpp_properties.json 	修改：     Home_Control_Center/app/blue_print/api/views.py

--- IoT_Controller_Project/Home_Control_Center/app/blue_print/api/views.py
+++ IoT_Controller_Project/Home_Control_Center/app/blue_print/api/views.py
@@ -10,17 +10,17 @@ from . import my_sender
 def api_send_command_to_device():
 
     if len(request.args) == 0:
-        return json.dumps({"result":"no args!"}).encode('utf8')
+        return json.dumps({"result":"no args!"}).encode('utf-8')
 
     get_data = request.args
 
-    command_message = {"device_name":get_data["device_name"],"command":get_data["command"]}
+    command_message = {"device_name":get_data["device_name"].encode('utf-8'),"command":get_data["command"].encode('utf-8')}
     my_sender.broadcast(command_message)
 
     times = 0
     feedback_message = my_receiver.receive()
     print("api print: feedback_message is: ",feedback_message,'\n \n')
-    while (feedback_message == False) and (times < 100):
+    while (feedback_message == False) and (times < 10):
         feedback_message = my_receiver.receive()
         times += 1
         print("api print: feedback_message is: ",feedback_message)
