41780d4adfab639b5ce21ffcd21ce29660aaa442
倪旌哲 <1448976235@qq.com>
2022-01-23 14:51:05 +0000

    里程碑更新

--- Device_Driver/lib/device.h
+++ Device_Driver/lib/device.h
@@ -124,7 +124,7 @@ void device::run(){
 			task_state = this->processer.process(command);
 
 			JSONVar feedback;
-			feedback["device_name"] = "Center";
+			feedback["device_name"] = this->name;
 			feedback["command"] = "feedback";
 			JSONVar result;
 			result["device_state"] = this->device_state;
